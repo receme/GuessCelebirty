41c976a94cada61e9f5a530c5b96e127f847dced
mmsaddam <mmsaddam@gmail.com>
2016-04-30 22:28:05 +0600

    Change color, scroll vertical

--- GuessCelebrity/LabelViewController.m
+++ GuessCelebrity/LabelViewController.m
@@ -64,7 +64,7 @@
       CustomLabel *btn = [[CustomLabel alloc]initWithFrame:CGRectMake(xPos, yPos, kSize, kSize)];
       NSString *imageName = tag > [sharedController unlockLabelGet] ? @"lock" : @"unlock";
       [btn.labelBtn addTarget:self action:@selector(tap:) forControlEvents:UIControlEventTouchUpInside];
-      btn.tag = tag;
+      btn.labelTag.tag = tag;
       btn.labelTag.text = [NSString stringWithFormat:@"%zd",tag];
       [btn.labelBtn setBackgroundImage:[UIImage imageNamed:imageName] forState:UIControlStateNormal];
       tag++;
