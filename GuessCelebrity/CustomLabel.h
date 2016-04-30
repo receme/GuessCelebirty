2755aeec6afe50fe69b56a2185bac0f23f6d98d5
mmsaddam <mmsaddam@gmail.com>
2016-04-30 22:16:55 +0600

    change scrollview direction

---
+++ GuessCelebrity/CustomLabel.h
@@ -0,0 +1,17 @@
+//
+//  CustomLabel.h
+//  GuessCelebrity
+//
+//  Created by Md. Muzahidul Islam on 4/30/16.
+//  Copyright © 2016 Muzahid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomLabel : UIView
+
+@property(nonatomic, strong) UIButton *labelBtn;
+@property(nonatomic, strong) UILabel *labelTag;
+
+
+@end
